28c898624b7a222ddfcc4bd4cb12b1b532dd7409
Qingbo Zhang <ripple0328@gmail.com>
2023-09-25 11:24:37 -0700

    remove wakatime to be replaced by screentime

--- packages.el
+++ packages.el
@@ -6,7 +6,6 @@
 ;; (package! another-package :recipe (:fetcher github :repo "username/repo"))
 ;; (package! builtin-package :disable t)
 (package! easy-hugo)
-(package! wakatime-mode)
 (package! presentation)
 (package! org-jira)
 (package! org-modern)
